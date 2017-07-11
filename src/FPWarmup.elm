2753ad998963669ba92f7402aaf1311cbbd54847
Dan Brooks <dmbrooks@live.co.uk>
2017-07-11 20:58:28 +0100

    Initial implementation of homeworks

--- src/FPWarmup.elm
+++ src/FPWarmup.elm
@@ -5,27 +5,43 @@ module FPWarmup exposing
   , take
   )
 
--- Add some imports here if you'd like. ------------------------------
-
-----------------------------------------------------------------------
-
+import List exposing (foldr)
 
 digitsOfInt : Int -> List Int
 digitsOfInt n =
-  Debug.crash "TODO"
+  if n < 0 then []
+  else if n < 10 then [n]
+  else (digitsOfInt <| n // 10) ++ [n % 10]
 
 additivePersistence : Int -> Int
 additivePersistence n =
-  Debug.crash "TODO"
+  if n < 10 then 0
+  else 1 + (additivePersistence (sumDigits n))
 
 digitalRoot : Int -> Int
 digitalRoot n =
-  Debug.crash "TODO"
+  if n < 0 then 0
+  else if n < 10 then n
+  else digitalRoot (sumDigits n)
+
+sumDigits : Int -> Int
+sumDigits = foldr (+) 0 << digitsOfInt
 
 subsequences : List a -> List (List a)
 subsequences xs =
-  Debug.crash "TODO"
+  case xs of
+    [] -> [[]]
+    (h :: t) -> (List.map ((::) h) <| subsequences t) ++ (subsequences t)
 
 take : Int -> List a -> Result String (List a)
 take k xs =
-  Debug.crash "TODO"
+  if k > 0 then
+    case xs of
+      [] -> Err "not enough elements"
+      (h :: t) -> case (take (k - 1) t) of
+        Ok ys -> Ok (h :: ys)
+        x -> x
+  else if k < 0 then
+    Err "negative index"
+  else
+    Ok []
