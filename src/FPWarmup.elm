4cedc40d47783f771f3d454aaa8376620bbd2ffc
Dan Brooks <dmbrooks@live.co.uk>
2017-07-11 13:46:42 +0100

    Download FPWarmup.elm

---
+++ src/FPWarmup.elm
@@ -0,0 +1,31 @@
+module FPWarmup exposing
+  ( digitsOfInt
+  , additivePersistence, digitalRoot
+  , subsequences
+  , take
+  )
+
+-- Add some imports here if you'd like. ------------------------------
+
+----------------------------------------------------------------------
+
+
+digitsOfInt : Int -> List Int
+digitsOfInt n =
+  Debug.crash "TODO"
+
+additivePersistence : Int -> Int
+additivePersistence n =
+  Debug.crash "TODO"
+
+digitalRoot : Int -> Int
+digitalRoot n =
+  Debug.crash "TODO"
+
+subsequences : List a -> List (List a)
+subsequences xs =
+  Debug.crash "TODO"
+
+take : Int -> List a -> Result String (List a)
+take k xs =
+  Debug.crash "TODO"
