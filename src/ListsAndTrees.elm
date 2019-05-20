000383303911211014302dba27a3edd793ab8942
Dan Brooks <dmbrooks@live.co.uk>
2019-05-20 21:20:08 +0100

    Add ListsAndTrees skeleton file

---
+++ src/ListsAndTrees.elm
@@ -0,0 +1,65 @@
+module ListsAndTrees
+    exposing
+        ( almostCompleteTrees
+        , balancedTree
+        , balancedTrees
+        , completeTrees
+        , fullTree
+        , mem
+        , suffixes
+        )
+
+------------------------------------------------------------------------------
+-- Problem 1
+------------------------------------------------------------------------------
+
+
+suffixes : List a -> List (List a)
+suffixes xs =
+    []
+
+
+
+------------------------------------------------------------------------------
+-- Problem 2
+------------------------------------------------------------------------------
+
+
+type Tree a
+    = Empty
+    | Node a (Tree a) (Tree a)
+
+
+mem : comparable -> Tree comparable -> Bool
+mem _ _ =
+    False
+
+
+fullTree : a -> Int -> Tree a
+fullTree _ _ =
+    Empty
+
+
+balancedTree : a -> Int -> Tree a
+balancedTree _ _ =
+    Empty
+
+
+create2 : a -> Int -> ( Tree a, Tree a )
+create2 _ _ =
+    ( Empty, Empty )
+
+
+balancedTrees : a -> Int -> List (Tree a)
+balancedTrees _ _ =
+    []
+
+
+completeTrees : a -> Int -> List (Tree a)
+completeTrees _ _ =
+    []
+
+
+almostCompleteTrees : a -> Int -> List (Tree a)
+almostCompleteTrees _ _ =
+    []
